960f58c8f6b070cf2cba37962371619e4f4f45b5
RobertHan96 <yshan4329@gmail.com>
2020-05-11 16:39:39 +0900

    2nd commit : 웹뷰 연결, 관공서 담당업무를 보여줄 리스트뷰 구현

--- app/src/main/java/com/studiohana/seoulgovofficeinfo/OfficeIntroActivity.kt
+++ app/src/main/java/com/studiohana/seoulgovofficeinfo/OfficeIntroActivity.kt
@@ -2,17 +2,27 @@ package com.studiohana.seoulgovofficeinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import com.studiohana.seoulgovofficeinfo.adapters.TaskAdapters
 import com.studiohana.seoulgovofficeinfo.datas.OfficeTask
 import kotlinx.android.synthetic.main.activity_office_intro.*
 
-class OfficeIntroActivity : AppCompatActivity() {
+class OfficeIntroActivity : BaseActivity() {
     val tasks = ArrayList<OfficeTask>()
     var mTaskAdapter : TaskAdapters? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_office_intro)
+        setupEvents()
+        setValues()
+    }
 
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
         tasks.add(OfficeTask(1,"연말정산"))
         tasks.add(OfficeTask(1,"소득 증명원 발급"))
         tasks.add(OfficeTask(1,"부가가치세 신고"))
@@ -20,5 +30,7 @@ class OfficeIntroActivity : AppCompatActivity() {
 
         mTaskAdapter = TaskAdapters(this, R.layout.task_list_item, tasks)
         taskListView.adapter = mTaskAdapter
+
     }
+
 }
