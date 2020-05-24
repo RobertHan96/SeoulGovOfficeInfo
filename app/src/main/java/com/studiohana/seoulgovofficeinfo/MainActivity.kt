03e90ebcf3d559b210739d4ced717379db5d1048
RobertHan96 <yshan4329@gmail.com>
2020-05-24 19:22:04 +0900

    14th commit : Firebase analytics 연동

--- app/src/main/java/com/studiohana/seoulgovofficeinfo/MainActivity.kt
+++ app/src/main/java/com/studiohana/seoulgovofficeinfo/MainActivity.kt
@@ -3,13 +3,15 @@ package com.studiohana.seoulgovofficeinfo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
-
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseAnalytics = FirebaseAnalytics.getInstance(mContext)
         setupEvents()
         setValues()
     }
@@ -43,5 +45,13 @@ class MainActivity : BaseActivity() {
     override fun setValues() {
 
     }
+
+//    fun sendUserSelectItemEvents(selectedItem : String) {
+//        val bundle = Bundle()
+//        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, id)
+//        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, name)
+//        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image")
+//        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+//    }
 
 }
